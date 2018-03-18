0d76825f66b8d4c2d9286eaf79126a862dbb7f95
Luis Vera Pomez <kikelvp@videotax.com>
2018-03-17 22:56:22 -0600

    Added new project

---
+++ GitPractice3/GitPractice3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractice3
+//
+//  Created by Luis Vera Pomez on 2018-03-17.
+//  Copyright © 2018 vtn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
